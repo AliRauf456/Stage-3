7f11de41f69474e00947bdbde481b6834f312984
A.Rauf456 <Ali.Rauf089@gmail.com>
2024-04-11 15:53:39 +0100

    r

--- create-account.php
+++ create-account.php
@@ -1,5 +1,5 @@
 <?php
-$database_file = "/Applications/XAMPP/xamppfiles/htdocs/Stage-3";
+$database_file = "/Applications/XAMPP/xamppfiles/htdocs/Stage-3/MortgageSystem.sqbpro";
 $db = new SQLite3($database_file);
 if(!$db) {
     die("Connection failed: " . $db->lastErrorMsg());
