edbbcad6b0709c85a9b4d5f4de249a04aee6a68a
A.Rauf456 <Ali.Rauf089@gmail.com>
2024-04-11 15:27:13 +0100

    change

--- create-account.php
+++ create-account.php
@@ -5,10 +5,6 @@ if(!$db) {
     die("Connection failed: " . $db->lastErrorMsg());
 }
 
-$database_file = "your_database_file.db"; 
-$db = new SQLite3($database_file);
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $firstname = $_POST['firstName'];
@@ -17,23 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirmPassword'];
 
-    
     if ($password !== $confirmPassword) {
         echo "Passwords do not match.";
         exit();
     }
 
-  
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    
     $stmt = $db->prepare("INSERT INTO user (firstname, surname, email, password) VALUES (:firstname, :surname, :email, :password)");
     $stmt->bindParam(':firstname', $firstname);
     $stmt->bindParam(':surname', $surname);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':password', $hashed_password);
 
-    
     $result = $stmt->execute();
 
     if ($result) {
